8dfb75144c6d2200591405be66cf5960e7522905
Richard Hounslow <richard@overdose.digital>
2023-09-13 18:15:24 +1000

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <h1>GX Chargify POC 1 - BE/API/Proxy</h1>;
+  return (
+    <div className="flex items-start h-screen">
+      <h1>GX Chargify POC 1 - BE/API/Proxy</h1>
+    </div>
+  );
 }
